5fc4691ec043cdc3650901fdbad629fbe832eb93
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-14 16:32:35 +0530

    fixed linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """Add two numbers"""
     return x+y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """Subtract x from y"""
     return y-x
