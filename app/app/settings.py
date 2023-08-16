a18cb790b44ac95fcca8cf681cada9066c4a54a6
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-16 17:07:53 +0530

    created custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&am&2cxh^u7_hln!df-&&)d4!afx%644&yann^71&wqt#c!oz4'
+SECRET_KEY = 'django-insecure-&am&2cxh^u7_hln!df-&&)d4!afx%644&yann^71&wqt#c!oz4'  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -127,3 +127,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
