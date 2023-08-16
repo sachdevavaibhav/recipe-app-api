6f903dbb04e7d6e9636b702aa1b8b14a038a1cf0
Vaibhav Sachdeva <sachdevavaibhav.2001@gmail.com>
2023-08-16 22:03:02 +0530

    email empty check

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,8 @@ class ModelTests(TestCase):
         for email, excepted in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, excepted)
+
+    def test_new_user_without_email_raised_error(self):
+        """Test that creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
